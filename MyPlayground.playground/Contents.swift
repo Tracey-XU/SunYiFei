27bcfa853a31419d74838e503c3fbd796cfedcef
syf <mac@10.0.0.25>
2015-11-10 12:08:01 +0800

    playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello"
+//var str_1 = ",Jacky Sun"
+//var name = str + str_1
+//
+//
+//var hand = 2
+//var age = 23
+//// 转为字符chuan
+//var handStr = String(hand)
+//var say = "I have \(hand) hands , I age is \(age)"
+//
+//// var 代表变量 let 代表常量
+//var 😃: Float = 33.0
+//
+//UInt8.max
+//Int8.min
+//Int8.max
+
+
+//let dfdf: Float = 3.24
+//
+//let money = 100_0000
+
+// 范围远算符 swift 里面赋值运算符没有返回值
+for i in 0..<5{
+    println(i)
+}
+
+//溢出运算符
+
+//let x = UInt8.max
+//let y = x &+ 2
+//let z = x &* 2
+
+//var btn = UIButton()
+//btn.frame = CGRectMake(0, 0, 100, 100)
+//btn.backgroundColor = UIColor.redColor()
+
+var str = "ddd"
+var str2 = "dsd"
+var set = str+str2
+
+
+
+
+
+
